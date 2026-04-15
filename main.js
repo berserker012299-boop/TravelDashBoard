cc50f0536f1efbc0d0a5dbd6c0545fed1bbcff2e
Tu Nombre <tuemail@example.com>
2026-04-15 23:03:06 +0200

    feat(js): add objeto ciudades

---
+++ main.js
@@ -0,0 +1,8 @@
+//objetos de ciudades 
+const ciudades = {
+  barcelona: { nombre: "Barcelona", pais: "España",          lat: 41.38, lon: 2.17,    moneda: "EUR" },
+  london:    { nombre: "London",    pais: "United Kingdom",  lat: 51.50, lon: -0.12,   moneda: "GBP" },
+  paris:     { nombre: "Paris",     pais: "Francia",         lat: 48.85, lon: 2.35,    moneda: "EUR" },
+  new_york:  { nombre: "New York",  pais: "Estados Unidos",  lat: 40.71, lon: -74.00,  moneda: "USD" },
+  tokyo:     { nombre: "Tokyo",     pais: "Japón",           lat: 35.67, lon: 139.65,  moneda: "JPY" },
+};
